6115fe26889c20c353a43923a5fd8a9bb28dfa77
Sinan <sinanaydogan@hotmail.com.tr>
2021-02-28 14:55:41 +0300

    FontAwesome Support Added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/index.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* Icons */
+import { faRocket } from '@fortawesome/free-solid-svg-icons'
+library.add(faRocket)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
