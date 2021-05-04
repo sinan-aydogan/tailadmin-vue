eb1c94b83d8de8486c0d514599c96cc86cd6fc4f
sinan-aydogan <sinanaydogan@hotmail.com.tr>
2021-05-04 13:26:06 +0300

    StatisticWidget component base added

---
+++ src/components/Card/StatisticWidget.vue
@@ -0,0 +1,17 @@
+<template>
+  <main-layout></main-layout>
+</template>
+
+<script>
+import MainLayout from "@/layouts/MainLayout";
+export default {
+  name: "StaticWidget",
+  components : {
+    MainLayout
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/StatisticWidgets.vue
@@ -0,0 +1,17 @@
+<template>
+  <main-layout></main-layout>
+</template>
+
+<script>
+import MainLayout from "@/layouts/MainLayout";
+export default {
+  name: "StaticWidgets",
+  components : {
+    MainLayout
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
