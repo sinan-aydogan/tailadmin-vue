d2a5f14d3c2e97c8870be0f1cb83932030e09781
sinan-aydogan <sinanaydogan@hotmail.com.tr>
2021-05-10 03:06:44 +0300

    Sub menu open/close scroll effect added

--- src/layouts/LeftMenuItem.vue
+++ src/layouts/LeftMenuItem.vue
@@ -4,33 +4,41 @@
     <router-link
         @click.native="showingSubMenu = !showingSubMenu ; $emit('showLeftMenuEmit')"
         :class="{
-        'justify-between': showingLeftMenu === 'true',
+        'justify-start': showingLeftMenu === 'true',
         'justify-center': showingLeftMenu === 'false',
-        'bg-white text-gray-700 hover:bg-blue-300': showingSubMenu,
-        'text-white hover:text-gray-700 hover:bg-gray-100': !showingSubMenu,
+        'bg-white text-gray-700 rounded-md': showingSubMenu,
+        'text-white hover:text-gray-700 hover:bg-gray-100 rounded-md': !showingSubMenu,
       }"
         class="left-menu-item"
      :to="item.link">
-      <!-- Label -->
-      <div v-if="showingLeftMenu === 'true'">{{ item.label }}</div>
       <!-- Icon -->
       <div>
-        <font-awesome-icon :icon="item.icon !== '' ? item.icon : 'dot-circle'" />
+        <font-awesome-icon v-if="item.icon" :icon="item.icon" />
       </div>
+      <!-- Label -->
+      <div
+          v-if="showingLeftMenu === 'true'"
+          :class="['w-full',item.icon ? 'ml-3':'']">
+        {{ item.label }}
+      </div>
+      <!--Indicator Icon-->
+      <font-awesome-icon v-if="item.items && !showingSubMenu && showingLeftMenu == 'true'" class="mr-1" icon="angle-down" />
+      <font-awesome-icon v-if="item.items && showingSubMenu && showingLeftMenu == 'true'" class="mr-1" icon="angle-left" />
     </router-link>
 
     <!-- Sub Item -->
+    <transition name="fade">
     <div
       v-show="showingSubMenu"
-      v-for="(item, index) in item.items"
-      :key="index"
-      class="flex flex-col mt-1"
+      class="flex flex-col mt-1 bg-gray-100 bg-opacity-10 rounded-md overflow-hidden"
     >
       <left-menu-sub-item
+        v-for="(item, index) in item.items"
+        :key="index"
         :item="item"
-        :showingLeftMenu="showingLeftMenu"
-      ></left-menu-sub-item>
+        :showingLeftMenu="showingLeftMenu"/>
     </div>
+    </transition>
   </div>
 </template>
 
@@ -48,3 +56,24 @@ export default {
   },
 };
 </script>
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: all 1s;
+}
+.fade-enter{
+  opacity: 0;
+  max-height: 0px;
+}
+.fade-enter-to{
+  opacity: 1;
+  max-height: 700px;
+}
+.fade-leave {
+  opacity: 1;
+  max-height: 700px;
+}
+.fade-leave-to {
+  opacity: 0;
+  max-height: 0px;
+}
+</style>
