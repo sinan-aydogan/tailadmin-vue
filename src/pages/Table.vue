275f4bb9e3af9f4b825a1fba0e0e1f8f70c6aa49
sinan-aydogan <sinanaydogan@hotmail.com.tr>
2021-05-12 12:50:52 +0300

    Table color selector added

--- src/pages/Table.vue
+++ src/pages/Table.vue
@@ -4,9 +4,35 @@
     <template #header>
       Tables
     </template>
+    <!--Subheader-->
+    <template #subHeader>
+      Customizable tables
+    </template>
     <!--Content-->
     <template #default>
-      <t-table :content="sampleData.users" :header="header" color="yellow">
+      <div class="my-2 border rounded-md overflow-hidden">
+        <ul class="flex justify-center">
+          <li class="bg-red-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='red'">
+            <div v-if="tableColor=='red'" :class="selectorInnerStyle">Red</div>
+          </li>
+          <li class="bg-blue-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='blue'">
+            <div v-if="tableColor=='blue'" :class="selectorInnerStyle">Blue</div>
+          </li>
+          <li class="bg-indigo-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='indigo'">
+            <div v-if="tableColor=='indigo'" :class="selectorInnerStyle">Indigo</div>
+          </li>
+          <li class="bg-yellow-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='yellow'">
+            <div v-if="tableColor=='yellow'" :class="selectorInnerStyle">Yellow</div>
+          </li>
+          <li :class="{'bg-gray-600' : tableColor === 'black'}" class="bg-black p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='black'">
+            <div v-if="tableColor=='black'" :class="selectorInnerStyle">Black</div>
+          </li>
+          <li class="bg-gray-500 p-2 w-full h-10 items-center flex justify-center cursor-pointer" @click="tableColor='gray'">
+            <div v-if="tableColor=='gray'" :class="selectorInnerStyle">Gray</div>
+          </li>
+        </ul>
+      </div>
+      <t-table :color="tableColor" :content="sampleData.users" :header="header">
         <template #photo="{props}">
           <img :src="props.photo"/>
         </template>
@@ -29,10 +55,16 @@ export default {
   },
   data(){
     return {
+      tableColor : 'blue',
       sampleData : {
         users
       },
       header : ['name','email','age','photo']
+    }
+  },
+  computed:{
+    selectorInnerStyle(){
+      return 'flex-shrink-0 w-full text-center bg-opacity-50 px-2 py-1 bg-white rounded-full font-semibold z-10'
     }
   }
 }
