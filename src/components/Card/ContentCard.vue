381e543881512aca12a7b64cb3bd612a5fbf534d
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-13 21:56:19 +0300

    ContentCard border width fixed to 2

--- src/components/Card/ContentCard.vue
+++ src/components/Card/ContentCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
       class="p-4"
-      :class="[colorValue,widthValue,radiusStyle,{'border' : border},'relative']">
+      :class="[colorValue,widthValue,radiusStyle,{'border-2' : border},'relative']">
     <!--Card Title-->
     <div class="font-bold text-xl">
       <slot name="title"></slot>
@@ -46,11 +46,11 @@ export default {
         return 'bg-indigo-200 border-indigo-500';
       } else if (this.color === 'yellow') {
         return 'bg-yellow-200 border-yellow-500';
-      } else if (this.green === 'green') {
+      } else if (this.color === 'green') {
         return 'bg-green-200 border-green-500';
-      } else if (this.green === 'gray') {
+      } else if (this.color === 'gray') {
         return 'bg-gray-200 border-gray-500';
-      } else if (this.green === 'black') {
+      } else if (this.color === 'black') {
         return 'bg-gray-700 border-black text-gray-200';
       } else {
         return 'bg-white'
