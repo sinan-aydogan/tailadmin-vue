9ab29b65e18283dded19c810f279b0ca01acc120
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-05-23 16:32:12 +0300

    Avatar indicator size fixed

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -1,8 +1,14 @@
 <template>
   <div :class="[team === true ? '-ml-4 overflow-hidden' : 'relative']">
     <img :class="[radiusStyle, sizeStyle, team === true ? 'border-2 border-white border-opacity-50' : '']" :src="link">
-    <span v-if="indicator" :class=" ['absolute items-center justify-center font-semibold text-xs text-white w-1/3 h-1/3 flex rounded-full',indicatorStyle]">
-      <span v-if="size > 2">{{ indicator.label}}</span>
+    <span
+        v-if="indicator"
+        :class="[
+                'absolute flex justify-center items-center w-1/3 h-1/3 rounded-full text-white font-semibold',
+                size<7 ? 'text-xs' : 'text-normal',
+                indicatorStyle
+                ]">
+            <span v-text="size>2 ? indicator.label : null"/>
     </span>
   </div>
 </template>
